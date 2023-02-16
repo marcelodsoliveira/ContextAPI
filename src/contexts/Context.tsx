348bf01829013b63f279085cf960903605a31cc0
Marcelo dos Santos Oliveira <oliveira.lnx@gmail.com>
2023-02-16 16:44:22 -0300

    Created themeReducer.ts and solved a problem with type

--- src/contexts/Context.tsx
+++ src/contexts/Context.tsx
@@ -1,9 +1,11 @@
 import { createContext, useReducer } from "react";
 import { UserType, userInitialState, userReducer } from "../reducers/userReducer";
+import { ThemeType, themeInitialState, themeReducer } from "../reducers/themeReducer";
 import { reducerActionType } from "../types/reducerActionType";
 
 type initialStateType = {
   user: UserType;
+  theme: ThemeType;
 }
 
 type ContextType = {
@@ -12,7 +14,8 @@ type ContextType = {
 }
 
 const initialState = {
-  user: userInitialState
+  user: userInitialState,
+  theme: themeInitialState
 }
 
 export const Context = createContext<ContextType>({
@@ -21,10 +24,11 @@ export const Context = createContext<ContextType>({
 });
 
 const mainReducer = (state: initialStateType, action: reducerActionType) => ({
-  user: userReducer(state.user, action)
+  user: userReducer(state.user, action),
+  theme: themeReducer(state.theme, action)
 });
 
-export const ContextProvider: React.FC = ({ children }: React.PropsWithChildren) => {
+export const ContextProvider= ({ children }: React.PropsWithChildren) => {
   const [state, dispatch] = useReducer(mainReducer, initialState)
   
   return (
